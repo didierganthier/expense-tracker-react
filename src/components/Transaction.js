0c5936db52057d975cff31d8e6a3f5f477a51d51
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-10-16 14:17:52 -0400

    Add plus/minus check

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Transaction = ({transaction}) => {
     return (
-        <li className="minus">
+        <li className={transaction.amount > 0 ? "plus" : "minus"}>
             {transaction.text} <span>{transaction.amount > 0? "+" : "-"}${transaction.amount}</span><button className="delete-btn">x</button>
         </li>
     )
