ff00b9556f4ea0c2ee3dd0f6572723a965a26895
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-10-16 18:29:32 -0400

    Add delete functionality

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 const Transaction = ({transaction}) => {
+    const { deleteTransaction } = useContext(GlobalContext);
+
+    const sign = transaction.amount < 0 ? '-' : '+';
+
     return (
         <li className={transaction.amount > 0 ? "plus" : "minus"}>
-            {transaction.text} <span>{transaction.amount > 0? "+" : "-"}${transaction.amount}</span><button className="delete-btn">x</button>
+            {transaction.text} 
+            <span>{sign}${Math.abs(transaction.amount)}</span>
+            <button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
         </li>
     )
 }
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -18,8 +18,17 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
+    // Actions
+    function deleteTransaction(id) {
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        });
+    }
+
     return (<GlobalContext.Provider value={{
         transactions: state.transactions,
+        deleteTransaction
     }}>
         {children}
     </GlobalContext.Provider>);
