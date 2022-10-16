f4ca0312322053fcbcecf2f20a490cc8c641fc0f
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-10-16 14:07:34 -0400

    Fix sign bug

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Transaction = ({transaction}) => {
     return (
         <li className="minus">
-            {transaction.text} <span>{transaction.amount? "+" : "-"}${transaction.amount}</span><button className="delete-btn">x</button>
+            {transaction.text} <span>{transaction.amount > 0? "+" : "-"}${transaction.amount}</span><button className="delete-btn">x</button>
         </li>
     )
 }
