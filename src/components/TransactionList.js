2d5c0307752690018241b5f552c24a8520e208f8
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-10-16 14:01:45 -0400

    Map transactions in TransactionList

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 const TransactionList = () => {
+    const { transactions } = useContext(GlobalContext);
+
     return (
         <>
             <h3>History</h3>
             <ul className="list">
-                 <li className="minus">
-                    Cash <span>-$400</span><button className="delete-btn">x</button>
-                </li> 
+                {transactions.map(transaction => (
+                    <li className="minus">
+                        {transaction.text} <span>${transaction.amount}</span><button className="delete-btn">x</button>
+                    </li>
+                ))}
             </ul>
         </>
     )
