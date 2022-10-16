faa84ec948a770f370431550c03bee848a0983fe
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-10-16 13:12:05 -0400

    Create Balance component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from "./components/Header";
 import "./App.css";
+import Balance from "./components/Balance";
 
 const App = () => {
   return (
     <div>
       <Header/>
+      <div className="container">
+        <Balance/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Balance.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Balance = () => {
+  return (
+    <div>
+      <h4>Your Balance</h4>
+      <h1 id='balance'>$0.00</h1>
+    </div>
+  )
+}
+
+export default Balance
